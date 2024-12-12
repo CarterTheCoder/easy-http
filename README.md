b58d330352c286a6ee0d93f1d32658d074d05e68
CarterTheCoder <75151313+CarterTheCoder@users.noreply.github.com>
2024-12-12 12:36:05 -0500

    Support for multiple HTML files added

--- README.md
+++ README.md
@@ -12,15 +12,14 @@ to run:
     $ cd release
     $ ./easy-http
 ### Config:
+Place your HTML files in the html directory.
+The root URI (e.g. localhost:8080/) will always serve index.html from the specified root directory.
 The default configuration file is located at:
 
     ~/.config/easy-http/default-config.toml
 and should look like this:
 
     bind_to = '127.0.0.1:8080' # What the server should bind to, e.g. 192.168.1.100:8080
-    html_path = 'index.html' # The path to the HTML file that should be served
+    html_path = 'html' # The root directory from which HTML files should be served
     not_found_path = '404.html' # The path to the 404 HTML file
 Change these options as needed.
-
-   
-
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    fs
+    fs,
+    path::Path,
 };
-use serde_derive::{
-    Serialize, Deserialize
-};
+
+use serde_derive::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 struct Config {
     bind_to: String,
-    html_path: String,
+    html_path: String, // This is now the root directory where HTML files are served from
     not_found_path: String,
 }
 
@@ -18,7 +18,7 @@ impl ::std::default::Default for Config {
     fn default() -> Self {
         Self {
             bind_to: "127.0.0.1:8080".to_string(),
-            html_path: "index.html".to_string(),
+            html_path: "html".to_string(),  // This is the directory containing HTML files
             not_found_path: "404.html".to_string(),
         }
     }
@@ -30,7 +30,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream, &cfg);
     }
     
@@ -40,19 +39,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn handle_connection(mut stream: TcpStream, cfg: &Config) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    
+    // Parse the URI from the request line
+    let parts: Vec<&str> = request_line.split_whitespace().collect();
+    let uri = if parts.len() > 1 { parts[1] } else { "/" };
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", cfg.html_path.to_string())
-    } else {    
-        ("HTTP/1.1 404 NOT FOUND", cfg.not_found_path.to_string())
+    // Determine the file path
+    let (status_line, filename) = if uri == "/" {
+        // Serve the index.html file in the root directory
+        ("HTTP/1.1 200 OK", format!("{}/index.html", cfg.html_path))
+    } else {
+        // For other URIs, serve the corresponding file in the html_path directory
+        let path = format!("{}{}", cfg.html_path, uri);
+        if Path::new(&path).exists() {
+            ("HTTP/1.1 200 OK", path)
+        } else {
+            // If the file is not found, serve the 404 page
+            ("HTTP/1.1 404 NOT FOUND", cfg.not_found_path.clone())
+        }
     };
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-
 }
+
